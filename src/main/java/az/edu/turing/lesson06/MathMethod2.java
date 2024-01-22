ab09563b50e3c8be9850b65fa45608000f37f2db
nurlanlan <nurahmad004@gmail.com>
2024-01-22 20:52:26 +0400

    version 1.0

---
+++ src/main/java/az/edu/turing/lesson06/MathMethod2.java
@@ -0,0 +1,9 @@
+package az.edu.turing.lesson06;
+
+public class MathMethod2 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("2*" + i + "=" + 2 * i);
+        }
+    }
+}
