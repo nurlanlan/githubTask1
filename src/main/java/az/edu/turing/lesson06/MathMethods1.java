63eff9cad2f1d3d6b3c806d244b57b99fac97496
nurlanlan <nurahmad004@gmail.com>
2024-01-22 19:30:24 +0400

    version 1.0

---
+++ src/main/java/az/edu/turing/lesson06/MathMethods1.java
@@ -0,0 +1,12 @@
+package az.edu.turing.lesson06;
+
+import java.util.Scanner;
+
+public class MathMethods1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a%b);
+    }
+}
