a09d5e7410bf49f035279e4372fff723fa189c09
nurlanlan <nurahmad004@gmail.com>
2024-01-22 20:12:55 +0400

    version 1.0

--- src/main/java/az/edu/turing/lesson06/IdentifyMonthByNumber.java
+++ src/main/java/az/edu/turing/lesson06/IdentifyMonthByNumber.java
@@ -11,24 +11,27 @@ public class IdentifyMonthByNumber {
             System.out.println("January");
         } else if (NumbeOfMonth ==2) {
             System.out.println("Feburary");
-        }else if (NumbeOfMonth ==2) {
+        }else if (NumbeOfMonth ==3) {
             System.out.println("March");
-        }else if (NumbeOfMonth ==2) {
+        }else if (NumbeOfMonth ==4) {
             System.out.println("April");
-        }else if (NumbeOfMonth ==2) {
+        }else if (NumbeOfMonth ==5) {
             System.out.println("May");
-        }else if (NumbeOfMonth ==2) {
+        }else if (NumbeOfMonth ==6) {
             System.out.println("June");
-        }else if (NumbeOfMonth ==2) {
+        }else if (NumbeOfMonth ==7) {
             System.out.println("July");
-        }else if (NumbeOfMonth ==2) {
+        }else if (NumbeOfMonth ==8) {
             System.out.println("August");
-        }else if (NumbeOfMonth ==2) {
+        }else if (NumbeOfMonth ==9) {
             System.out.println("September");
-        }else if (NumbeOfMonth ==2) {
+        }else if (NumbeOfMonth ==10) {
             System.out.println("October");
-        }else if (NumbeOfMonth ==2) {
+        }else if (NumbeOfMonth ==11) {
             System.out.println("Novemver");
-        }else System.out.println("December");
+
+        }else if (NumbeOfMonth ==12) {
+            System.out.println("December");
+        }
     }
 }
